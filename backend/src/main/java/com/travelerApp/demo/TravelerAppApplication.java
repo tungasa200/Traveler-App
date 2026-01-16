09a116c313327929af2eee785b73a9f21212da4a
tung_asa200 <ksoung140w@gmail.com>
2026-01-16 16:43:11 +0900

    initial commit : 최초 설정 및 개발 환경 세팅

---
+++ backend/src/main/java/com/travelerApp/demo/TravelerAppApplication.java
@@ -0,0 +1,13 @@
+package com.travelerApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelerAppApplication.class, args);
+	}
+
+}
